2fc7737e61c08e3eed296a7e1ba27e03684fe332
Marlie Ford <jive.hive@outlook.com>
2024-01-15 19:29:41 -0600

    added date to each current day

--- script.js
+++ script.js
@@ -67,37 +67,43 @@ function getCityByLatLon(lat, lon) {
         document.querySelector(".windSpeed").innerHTML =  "wind-speed: " + data.list[0].wind.speed + " mph";
         document.querySelector(".description").innerHTML = data.list[0].weather[0].description;
         document.querySelector('.icon').src = "http://openweathermap.org/img/wn/"+ data.list[0].weather[0].icon +".png";
+        document.querySelector(".date").innerHTML = "" + moment().format("dddd, MMMM Do, YYYY");
+
 
         document.querySelector(".tempF1").textContent = convertKelvinToFahrenheit(data.list[1].main.temp) + "°F";
         document.querySelector(".humidity1").innerHTML = "humidity: " + data.list[1].main.humidity + "%";
         document.querySelector(".windSpeed1").innerHTML = "wind-speed: " + data.list[1].wind.speed + " mph";
         document.querySelector(".description1").innerHTML = data.list[1].weather[0].description;
         document.querySelector('.icon1').src = "http://openweathermap.org/img/wn/"+ data.list[1].weather[0].icon +".png";
+        document.querySelector(".date1").innerHTML = "" + moment().add(1,'days').format("dddd, MMMM Do");
 
         document.querySelector(".tempF2").textContent = convertKelvinToFahrenheit(data.list[2].main.temp) + "°F";
         document.querySelector(".humidity2").innerHTML = "humidity: " + data.list[2].main.humidity + "%";
         document.querySelector(".windSpeed2").innerHTML = "wind-speed: " + data.list[2].wind.speed + " mph";
         document.querySelector(".description2").innerHTML = data.list[2].weather[0].description;
         document.querySelector('.icon2').src = "http://openweathermap.org/img/wn/"+ data.list[2].weather[0].icon +".png";
+        document.querySelector(".date2").innerHTML = "" + moment().add(2,'days').format("dddd, MMMM Do");
 
         document.querySelector(".tempF3").textContent = convertKelvinToFahrenheit(data.list[3].main.temp) + "°F";
         document.querySelector(".humidity3").innerHTML = "humidity: " + data.list[3].main.humidity + "%";
         document.querySelector(".windSpeed3").innerHTML = "wind-speed: " + data.list[3].wind.speed + " mph";
         document.querySelector(".description3").innerHTML = data.list[3].weather[0].description;
         document.querySelector('.icon3').src = "http://openweathermap.org/img/wn/"+ data.list[3].weather[0].icon +".png";
+        document.querySelector(".date3").innerHTML = "" + moment().add(3,'days').format("dddd, MMMM Do");
 
         document.querySelector(".tempF4").textContent = convertKelvinToFahrenheit(data.list[4].main.temp) + "°F";
         document.querySelector(".humidity4").innerHTML = "humidity: " + data.list[4].main.humidity + "%";
         document.querySelector(".windSpeed4").innerHTML = "wind-speed: " + data.list[4].wind.speed + " mph";
         document.querySelector(".description4").innerHTML = data.list[4].weather[0].description;
         document.querySelector('.icon4').src = "http://openweathermap.org/img/wn/"+ data.list[4].weather[0].icon +".png";
+        document.querySelector(".date4").innerHTML = "" + moment().add(4,'days').format("dddd, MMMM Do");
 
         document.querySelector(".tempF5").textContent = convertKelvinToFahrenheit(data.list[5].main.temp) + "°F";
         document.querySelector(".humidity5").innerHTML = "humidity: " + data.list[5].main.humidity + "%";
         document.querySelector(".windSpeed5").innerHTML = "wind-speed: " + data.list[5].wind.speed + " mph";
         document.querySelector(".description5").innerHTML = data.list[5].weather[0].description;
         document.querySelector('.icon5').src = "http://openweathermap.org/img/wn/"+ data.list[5].weather[0].icon +".png";
-
+        document.querySelector(".date5").innerHTML = "" + moment().add(5,'days').format("dddd, MMMM Do");
     })
 }
 function convertKelvinToFahrenheit(valNum) 
